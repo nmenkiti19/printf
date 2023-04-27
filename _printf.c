cc1ecad63c870e23db521f148b45aa3b3c7ff841
nmenkiti19 <127206091+nmenkiti19@users.noreply.github.com>
2023-04-27 21:33:34 +0100

    Revert "printf"

--- _printf.c
+++ _printf.c
@@ -1,44 +1,84 @@
 #include "main.h"
+int (*find_format(const char *format))(va_list);
 
 /**
- * _printf - printf function
- * @format: const char pointer
- * Return: b_len
+ * find_format - checks if there is a valid format specifier
+ * @format: possible valid format specifier
+ * Return: pointer to valid function or NULL
+ */
+int (*find_format(const char *format))(va_list)
+{
+	int i = 0;
+	conv_t specs[] = {
+		{"c", check_c},
+		{"s", check_s},
+		{"i", check_d},
+		{"d", check_d},
+		{"b", check_b},
+		{"u", check_u},
+		{"o", check_o},
+		{"x", check_x},
+		{"X", check_hX},
+		{"p", check_p},
+		/*{"S", check_S},*/
+		{"r", check_r},
+		{"R", check_R13},
+		{NULL, NULL}
+	};
+
+	for (; specs[i].dt != NULL; i++)
+	{
+		if (*(specs[i].dt) == *format)
+			break;
+	}
+	return (specs[i].f);
+}
+
+/**
+ * _printf - function for format printing
+ * @format: list of arguments to printing
+ * Return: Number of characters to printing
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	va_list vlist;
+	int (*f)(va_list);
+	unsigned int i = 0, indx = 0;
 
-	register int count = 0;
+	if (format == NULL)
+		return (-1);
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	va_start(vlist, format);
+	while (format && format[i])
 	{
-		if (*p == '%')
+		if (format[i] != '%')
 		{
-			p++;
-			if (*p == '%')
+			handle_print(format[i]);
+			indx++;
+			i++;
+			continue;
+		}
+		else
+		{
+			if (format[i + 1] == '%')
 			{
-				count += _putchar('%');
+				handle_print('%');
+				indx++;
+				i += 2;
 				continue;
 			}
-			while (get_flap(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			else
+			{
+				f = find_format(&format[i + 1]);
+				if (f == NULL)
+					return (-1);
+				i += 2;
+				indx += f(vlist);
+				continue;
+			}
+		}
+		i++;
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	va_end(vlist);
+	return (indx);
 }
